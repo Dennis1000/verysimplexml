db8cad711b2f2c1f027af5f22c1bd64f502e119e
dennis.spreen@gmail.com <dennis.spreen@gmail.com@983a6492-2d16-0977-eb5e-3abfc3cf358a>
2014-02-11 15:52:06 +0000

    added 2 fluent interfaces examples

---
+++ Examples/VCL/Example 3 - Fluent interfaces 2/Example3.dpr
@@ -0,0 +1,15 @@
+program Example3;
+
+uses
+  Vcl.Forms,
+  unit1 in 'unit1.pas' {Form1},
+  Xml.VerySimple in '..\..\..\Source\Xml.VerySimple.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Examples/VCL/Example 3 - Fluent interfaces 2/unit1.pas
@@ -0,0 +1,54 @@
+unit unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  Xml.VerySimple;
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  Xml: TXmlVerySimple;
+  BookNode: TXmlNode;
+begin
+  // Create a new XML document
+  Xml := TXmlVerySimple.Create;
+
+  // Add a new child node, the first child node is the DocumentElement
+  Xml.AddChild('books');
+
+   // Create child nodes for the author and title
+  Xml.DocumentElement.AddChild('book').SetAttribute('id', 'bk101').AddChild('author').
+    SetText('Gambardella, Matthew').Parent.AddChild('title').Text := 'XML Developer''s Guide';
+
+  // Write to memo, the first 3 chars are the unicode BOM
+  Memo1.Lines.Text := Xml.Text;
+
+  // Write to file
+  Xml.SaveToFile('example3.xml');
+
+  // And free resources
+  Xml.Free;
+end;
+
+end.
